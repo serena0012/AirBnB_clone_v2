26681187b901f4db16e35107d0979d7fb8f049e0
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-04-09 19:01:22 +0100

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
-# Bash script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+#set things up for deployment
+HTML_CONTENT=\
+"
+<html>
+    <head>
+        <title>A dumy page </title>
+    </head>
+    <body>
+        Hi Human, good to see you here
+    </body>
+</html>
+"
+NGINX_CONFIG=\
+"
+server {
+ 	listen	80;
+	location /hbnb_static/ {
+		 alias /data/web_static/current/;
+		 index index.html;
+	}
+}	
+"
+sudo apt-get -y update && sudo apt-get -y install nginx 
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared/
+                                                    
+echo -e "$HTML_CONTENT" > /data/web_static/releases/test/index.html
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+# create sym link, if it already exists, replace it.
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+echo -e "$NGINX_CONFIG" > /etc/nginx/sites-enabled/default
 
-sudo service nginx restart
+sudo service nginx restart 
